1fe7102459281850f2f75ea44a2c075a2f578369
Daiane <daianeksilva07@gmail.com>
2026-04-26 02:07:05 -0300

    header, tailwind.config e importação da fonte
    Construção base do header, importação da fonte lexend deca, configuração dela no arquivo tailwind.config.js e imagens

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["portfolio.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'lexend': ['"Lexend Deca"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
